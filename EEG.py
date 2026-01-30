461c1798c3f0d6935426e42de442ea6f3198bf2b
Reese's Puffs <160973417+PhantomXReborn@users.noreply.github.com>
2026-01-30 09:03:41 -0500

    version_1

---
+++ EEG.py
@@ -0,0 +1,30 @@
+'''
+Program: Emotion-to-Environment Generator
+Author: Reece Hannah
+Language: Python
+Purpose: 
+The goal is to explore how emotions 
+shape perception and mood by translating
+internal feelings into external, 
+imagined spaces.
+'''
+
+def menu():
+    # Menu Screen
+    print("\n\tEmotion-to-Environment Generator") # Title
+
+    print("\n\nMade by: Reece Hannah") # Author
+
+    # Option Screen
+    print("\n\tWELCOME TO THE PROGRAM")
+    print("\nWhat would you wish to do?")
+    print("1)\tDisplay a list of emotions to choose from\n")
+    print("2)\tInput what emotion you are feeling\n")
+    print("3)\tAdd a new emotion and colour palettte\n")
+    print("4)\tDisplay a random emotion & colour palette\n\n")
+
+    # Input Screen
+    option = int(input("Enter your choice\n---> "))
+
+
+
