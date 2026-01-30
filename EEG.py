b67c784cf6165a035c0a9b238e45f05c3fc710f1
Reese's Puffs <160973417+PhantomXReborn@users.noreply.github.com>
2026-01-30 09:47:41 -0500

    version_2

--- EEG.py
+++ EEG.py
@@ -9,6 +9,9 @@ internal feelings into external,
 imagined spaces.
 '''
 
+# Function
+
+# Main Function
 def menu():
     # Menu Screen
     print("\n\tEmotion-to-Environment Generator") # Title
@@ -26,5 +29,52 @@ def menu():
     # Input Screen
     option = int(input("Enter your choice\n---> "))
 
+    return option
 
+# Main Function
+def op1():
+    # Menu Screen
+    print("\n\tEmotion-to-Environment Generator") # Title
 
+    print("\n\nOption:\nDisplay a list of emotions to choose from") # Option
+
+    f = open("Emotions.txt")
+    print("\n" + f.read())
+
+    print("\n\nWhich emotion would you like to choose from?\n")
+    choice = str(input("--->")).lower()
+# Main Function
+def op2():
+    # Menu Screen
+    print("\n\tEmotion-to-Environment Generator") # Title
+
+    print("\n\nOption:\nnput what emotion you are feeling") # Option
+
+# Main Function
+def op3():
+    # Menu Screen
+    print("\n\tEmotion-to-Environment Generator") # Title
+
+    print("\n\nOption:\nAdd a new emotion and colour palettte") # Option
+
+    # Main Function
+def op4():
+    # Menu Screen
+    print("\n\tEmotion-to-Environment Generator") # Title
+
+    print("\n\nOption:\nDisplay a random emotion & colour palette") # Option
+
+# Global Variables
+userChoice = menu()
+
+match userChoice:
+    case 1:
+        op1()
+    case 2:
+        op2()
+    case 3:
+        op3()
+    case 4:
+        op4()
+    case _:
+        menu()
