468e312e0ab4e3f64bd8029fb3e165461a01f9dd
Reese's Puffs <160973417+PhantomXReborn@users.noreply.github.com>
2026-02-02 19:19:03 -0500

    version_5

--- EEG.py
+++ EEG.py
@@ -12,6 +12,7 @@ imagined spaces.
 # Imports
 import os
 import linecache
+import random
 from time import sleep
 
 # clear_screen Function
@@ -54,14 +55,14 @@ def op1():
     print("\n\nOption:\nDisplay a list of emotions to choose from") # Option
 
     f1 = open("Emotions.txt")
-    print("\n" + f.read())
+    print("\n" + f1.read())
 
     print("\n\nWhich emotion would you like to choose?\n")
     choice = str(input("--->")).lower()
 
     for x in f1:
         if choice == x:
-            print(linecache.getline('Palettes.txt', x)
+            print(linecache.getline('Palettes.txt', x))
 
 # op2 Function
 def op2():
@@ -72,12 +73,13 @@ def op2():
 
     emotion = str(input("\n---> "))
 
-    f1 = open("Emotions.txt")
+    f1 = open("Descript.txt")
 
+    print("\n\nDescription:\n")
     for x in f1:
         if choice == x:
-            print(linecache.getline('Palettes.txt', x)
-    
+            for i in range(7):
+                print(linecache.getline('Descript.txt', (x*7 + i)))
 
 # Main Function
 def op3():
@@ -86,12 +88,29 @@ def op3():
 
     print("\n\nOption:\nAdd a new emotion and colour palettte") # Option
 
-    # Main Function
+    emotion_to_append = atr(input("\nEmotion ---> "))
+    print("\n")
+    palette_to_append = atr(input("\nPalette---> "))
+
+    f1 = open("Emotions.txt")
+    f1.write(emotion_to_append + '\n')
+    f1.write(palette_to_append + '\n')
+
+    print("\n" + f1.read())
+
+# Main Function
 def newEmotion():
     # Menu Screen
     print("\n\tEmotion-to-Environment Generator") # Title
 
     print("\n\nOption:\nDisplay a random emotion & colour palette") # Option
+
+    with open("Emotions.txt", 'r') as f:
+        lines = len(f.readlines())
+
+    random_number = random.randint(1, lines)
+    print(linecache.getline('Emotions.txt', random_number))
+    print("\n" + linecache.getline('Palettes.txt', random_number))
 
 # Global Variables
 userChoice = menu()
